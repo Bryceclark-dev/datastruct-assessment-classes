3439a3c7a61ae5738572673261eaf27cb5ea88ae
Bryceclark-dev <brycejclark@sbcglobal.net>
2022-06-20 07:51:23 -0500

    Complete assessment

--- src/main/java/com/kenzie/app/Account.java
+++ src/main/java/com/kenzie/app/Account.java
@@ -1,46 +1,67 @@
 package com.kenzie.app;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class Account {
     //create class instance variables here
-
+    private String userName;
+    private String accountId;
+    private double balance;
 
     //implement the following class constructors
     public Account(String userName) {
+        this.userName = userName;
 
+        Random rand = new Random();
+        int id = rand.nextInt(999999);
+        String idNumber =  String.format("%06d", id);
+        this.accountId = userName.toLowerCase().substring(0, 3).concat(idNumber);
+
+        this.balance = 0.0;
     }
 
     public Account(String userName, String accountId) {
-
+        this.userName = userName;
+        this.accountId = accountId;
+        this.balance = 0.0;
     }
 
     public Account(String userName, String accountId, double balance) {
-
+        this.userName = userName;
+        this.accountId = accountId;
+        this.balance = balance;
     }
 
     public String getUserName() {
         // return the user name of the current account
-        return "";
+        return userName;
     }
 
     public String getAccountId() {
         // return the account id fo the current account
-        return "";
+        return accountId;
     }
 
     public double getBalance() {
         // return the current balance of the account
-        return 0.0;
+        return balance;
     }
 
     public void makeDeposit(double amount) {
         // Add the given amount to the balance of the account
-
+        balance += amount;
     }
 
     public double makeWithdrawal(double amount) {
         // If the account has enough money, then withdraw the given amount
         // If the account does not have enough money, then return 0 and do not modify the balance
         // Otherwise return the amount that was withdrawn
-       return 0;
+       if(balance >= amount){
+           balance -= amount;
+       }else{
+           return 0;
+       }
+       return amount;
     }
 }
